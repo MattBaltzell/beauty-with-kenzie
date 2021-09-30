2a449bfc7dbde48728699078bb93f251b7a521d3
zellified <76706476+zellified@users.noreply.github.com>
2021-09-30 10:35:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function modalOpen(){
   const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
   const body = document.body;
   body.style.position = 'fixed';
-  body.style.top = `${scrollY}px`;  
+  body.style.top = `-${scrollY}px`;  
   modal.classList.remove("hidden");
 }
 
@@ -44,13 +44,13 @@ function modalOpen(){
 // }
 
 function modalClose() {
-  
+  modal.classList.add("hidden");
   const body = document.body;
   const scrollY = body.style.top;
   body.style.position = '';
   body.style.top = '';
   window.scrollTo(0, parseInt(scrollY || '0') * -1);
-  modal.classList.add("hidden");
+  
 }
 
 function menuClose() {
