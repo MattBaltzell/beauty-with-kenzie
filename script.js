abdd415020622c091b83d4410b004347f5f7d344
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2021-10-06 16:59:43 -0500

    Refactoring js

--- script.js
+++ script.js
@@ -6,21 +6,15 @@ const modal = document.querySelector(".slideshow-modal");
 const modalX = document.querySelector("#modal-close");
 const galleryImages = document.querySelectorAll(".gallery-image");
 const galleryViewport = document.querySelector("#slideshow-display");
-const imgArr = [];
-let curSource;
-let curImage = 1;
+const imgArr = []
 const nextBtn = document.querySelector("#next-btn");
 const prevBtn = document.querySelector("#prev-btn");
 const imgNumber = document.querySelector("#image-number");
+let curSource; curImage = 1;
 
 logoAnchor ? buildArrClickListener(logoAnchor, jumpToSection) : null;
 menuLink ? buildArrClickListener(menuLink, jumpToSection) : null;
-modalX ? buildElClickListener(modalX, modalClose) : null;
 galleryImages ? buildArrClickListener(galleryImages, modalOpen): null;
-
-function buildElClickListener(el, func) {
-  el.addEventListener("click", func);
-}
 
 function buildArrClickListener(arr, func) {
   for (i = 0; i < arr.length; i++) {
@@ -32,13 +26,6 @@ function clearURL() {
   if(window.location.pathname == '/pricing/') {
     window.history.replaceState({}, document.title, "/pricing/");
   } else window.history.replaceState({}, document.title, "/");
-}
-
-function jumpToSectionBIG() {
-  const sect = this.textContent.toLowerCase();
-  const target = document.getElementById(`${sect}`);
-  target.scrollIntoView(true);
-  clearURL();
 }
 
 function jumpToSection() {
@@ -56,17 +43,23 @@ function jumpToSection() {
 })();
 
 function modalOpen() {
-  galleryViewport.textContent = "";
-  let source = this.getAttribute("src");
+  setSrc(this);
+  modal.classList.remove("hidden");
+  preventScroll();
+}
+
+function setSrc(el){
+  let source = el.getAttribute("src");
   for (i = 0; i < imgArr.length; i++) {
     if (imgArr[i] === source) {
-      // can change the url below to include "-lg" to keep thumbnail img files smaller than displayed images
       galleryViewport.style.backgroundImage = `url('${imgArr[i]}')`;
       imgNumber.textContent = `${i + 1} / ${imgArr.length}`;
       curImage = i + 1;
     }
   }
-  modal.classList.remove("hidden");
+}
+
+function preventScroll(){
   const scrollY = document.documentElement.style.getPropertyValue("--scroll-y");
   const body = document.body;
   body.style.position = "fixed";
@@ -76,28 +69,33 @@ function modalOpen() {
 }
 
 function modalClose() {
+  modal.classList.add("hidden");
+  allowScroll();
+}
+
+function allowScroll(){
   const body = document.body;
   const scrollY = body.style.top;
   body.style.position = "";
   body.style.width = "";
   body.style.top = "";
   window.scrollTo(0, parseInt(scrollY || "0") * -1);
-  modal.classList.add("hidden");
   document.documentElement.style.scrollBehavior = "smooth";
 }
 
-function nextImage() {
-  curImage == 12 ? (curImage = 1) : curImage++;
+function changeImage(direction){
+  direction();
   let imgSrc = imgArr[curImage - 1];
   galleryViewport.style.backgroundImage = `url("${imgSrc}")`;
   imgNumber.textContent = `${curImage} / ${imgArr.length}`;
 }
 
-function prevImage() {
-  curImage == 1 ? (curImage = 12) : curImage--;
-  let imgSrc = imgArr[curImage - 1];
-  galleryViewport.style.backgroundImage = `url("${imgSrc}")`;
-  imgNumber.textContent = `${curImage} / ${imgArr.length}`;
+function nxt(){
+  curImage == imgArr.length ? (curImage = 1) : curImage++;
+}
+
+function prev(){
+  curImage == 1 ? (curImage = imgArr.length) : curImage--;
 }
 
 function menuClose() {
